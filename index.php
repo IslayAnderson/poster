12995024e72c09155aaf65fb28159dc5f1574749
islay anderson <islayanderson@outlook.com>
2023-08-20 18:44:30 +0100

    complete basic front-end

--- index.php
+++ index.php
@@ -1,4 +1,65 @@
-<?php
-<h1>hello world</h1>
+<?php $do_somthing = '' ?>
 
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Islay Anderson</title>
+    <link rel="stylesheet" href="style/style.css">
+  </head>
+  <body>
+    <header>
+        <h1 class="title">Islay Anderson<span>Engineering, Photography, Politics, Skating</span></h1>
+    </header>
+    <main>
+        <div class="row">
+            <div class="project headline">
+                <a href="https://photography.islayanderson.co.uk" class="section_headline">
+                    <h2>Photography</h2>
+                </a>
+                <div class="h_line"></div>
+                <div class="tag_cloud">
+                    <span>120mm</span>
+                    <span>35mm</span>
+                    <span>Color 400</span>
+                    <span>ColorPlus 200</span>
+                    <span>Delta 3200</span>
+                    <span>FP4 Plus</span>
+                    <span>Gold 200</span>
+                    <span>HP5 Plus</span>
+                    <span>Ilford</span>
+                    <span>Kodak</span>
+                    <span>Lomography</span>
+                    <span>Portra 400</span>
+                    <span>UltraMax 400</span>
+                    <span>XP2</span>
+                </div>
+            </div>
+            <div class="project proof">
+                <img src="https://photography.islayanderson.co.uk/i.php?/upload/2023/08/20/20230820141935-c6a72f48-xx.jpg">
+            </div>
+            <div class="project proof">
+                <img src="https://photography.islayanderson.co.uk/i.php?/upload/2023/08/20/20230820131817-082dd59d-la.jpg">
+            </div>
+            <div class="project headline">
+                <a href="https://blog.islayanderson.co.uk" class="section_headline">
+                    <h2>Engineering &amp; Politics Blog</h2>
+                </a>
+                <div class="h_line"></div>
+                <div class="lastest_entry">
+                    <h3>Public Performance: Cuban Family code referendum</h3>
+                    <p>On September the 22nd 6 million Cubans took part in a constitutional referendum to include some of the most progressive legislation regarding sexuality, gender and the family unit. Passing with a 74% turnout voting in favour at 67%.</p>
+                    <p>On September the 22nd 6 million Cubans took part in a constitutional referendum to include some of the most progressive legislation regarding sexuality, gender and the family unit.<br />Passing with a 74% turnout voting in favour at 67%.</p>
+                    <p>With code that extends familial responsibilities to other senior members such as grandparents or surrogate mothers acknowledging the existence of non-traditional family structures, legalises same sex marriage, allowing same sex parents to adopt and outlawing child marriage.</p>
+                    <p>The new Cuban family code leap frogs the so called progressive leaders in Europe with phrasing in such as &ldquo;Love and solidarity are the platforms and axes on which family relationships revolve&rdquo; decoupling love and family from the concepts of sex and gender.<br />Acknowledging the people who had been lost in the cracks of its legislation like young people with no access to services they need because they were part of a queer or non-traditional family.<br />And showing the rest of the world what true socialism can achieve.</p>
+                    <a href="" title="latest blog entry">permalink</a>
+                </div>
+            </div>
+        </div>
+    </main>
+
+	<script src="script/main.js"></script>
+  </body>
+</html>
